86dbefef59b0b4ba7ad66dadf36c64645f5cc4d7
YukiS-27 <yukis.job27@gmail.com>
2020-12-16 08:06:12 +0000

    first commit Rails05

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: session_params)
+    # user = User.find_by(email: params[:session][:email])
+    
+    if use && user.authenticate(password: session_params)
+    # if use && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+    
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+  
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  # ストロングパラメータ
+  def session_params
+    params.require(:session).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+end
