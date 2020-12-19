85ff8b24237bbdea6afa2739764e808d2cdb3140
YukiS-27 <yukis.job27@gmail.com>
2020-12-19 10:17:59 +0000

    first commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email])
     # 変更前のコード => user = User.find_by(email: params[:session][:email])
     
-    if user && user.authenticate(session_params)
+    if user && user.authenticate(session_params[:password])
     # 変更前のコード => if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, success: 'ログインに成功しました'
